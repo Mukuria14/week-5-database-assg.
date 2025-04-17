3dadc466c50f18b1f1defe3801c737d7f2d37960
wahu mukuria 14 <mukuria@students.uonbi.ac.ke>
2025-04-17 17:09:29 +0300

    Add files via upload
    done first part of indexing

---
+++ answers.mysql indexing assg..sql
@@ -0,0 +1,7 @@
+-- I have created a database called sales, created a table called customers , with columns including, name of the phone, phones available and quanitity of phones sold,
+ -- of phones bought by the customers. i want to index phones for easy search by the seller. 
+ -- afterwards i have inserted data into the customers table,and creatws an index on phone names idx_phone. then i have viewed the index.
+ 
+
+
+
